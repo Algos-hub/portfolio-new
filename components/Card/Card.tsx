eea50733adc4112c55e4a9b2f14899ffac86d033
Algos-hub <72273184+Algos-hub@users.noreply.github.com>
2024-06-24 20:00:58 +0200

    added functionality to the Card component where you can choose whether the inner contents are a list or not and made the background toggable

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -1,17 +1,29 @@
 import React, { ReactNode } from "react";
 import styles from "./Card.module.css";
 
+function List({ children }: Readonly<{ children: ReactNode }>): ReactNode {
+  return <ul className={styles.list}>{children}</ul>;
+}
+
 export default function Card({
   title,
+  list = false,
+  background = true,
   children,
 }: Readonly<{
   title: string;
+  list: boolean;
+  background: boolean;
   children: React.ReactNode;
 }>): ReactNode {
   return (
-    <div className={styles.card}>
+    <div
+      className={`${styles.card} ${!background ? styles.cardNoBackground : ""}`}
+    >
       <h2 className={styles.title}>{title}</h2>
-      <div className={styles.content}>{children}</div>
+      <div className={styles.content}>
+        {list ? <List>{children}</List> : children}
+      </div>
     </div>
   );
 }
